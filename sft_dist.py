74cbedb7a8be8db99b29ddde4f6ab63cddb9c31c
Amir Yazdanbakhsh <ayazdan@google.com>
2025-02-10 01:08:45 +0000

    Update sft_dist.py
    Removed num proc so hugging face does autotuning

--- sft_dist.py
+++ sft_dist.py
@@ -200,8 +200,8 @@ def _prepare_non_packed_dataloader(
         "remove_columns": dataset.column_names if remove_unused_columns else None,
         "batch_size": args.dataset_map_batch_size,
     }
-    if isinstance(dataset, datasets.Dataset):
-        map_kwargs["num_proc"] = args.dataset_num_proc  # this arg is not available for IterableDataset
+    # if isinstance(dataset, datasets.Dataset):
+    #     map_kwargs["num_proc"] = args.dataset_num_proc  # this arg is not available for IterableDataset
     tokenized_dataset = dataset.map(tokenize, **map_kwargs)
 
     return tokenized_dataset
